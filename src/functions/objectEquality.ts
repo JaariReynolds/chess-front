e3cc550ae54c91be899d894e84463717fa45cb50
Jaari <jaari.rey@gmail.com>
2024-08-15 23:13:34 +1000

    added equality check functions for Piece and Square

---
+++ src/functions/objectEquality.ts
@@ -0,0 +1,16 @@
+import { Piece, Square } from "../types/gameboard";
+
+export function arePiecesEqual(piece1: Piece, piece2: Piece): boolean {
+  return (
+    piece1.name === piece2.name &&
+    piece1.teamColour === piece2.teamColour &&
+    piece1.pieceValue === piece2.pieceValue &&
+    piece1.hasMoved === piece2.hasMoved &&
+    areSquaresEqual(piece1.square, piece2.square)
+  );
+}
+
+export function areSquaresEqual(square1: Square | null, square2: Square | null): boolean {
+  if (square1 == null || square2 == null) return false;
+  return square1.x == square2.x && square1.y == square2.y;
+}
