14efa330b6d7913f424d1ab1ee90d2aab6322cf8
Jaari <jaari.rey@gmail.com>
2024-08-16 14:13:23 +1000

    pieces now deselectable, getSquareOpacity function added

--- src/components/gameboard.tsx
+++ src/components/gameboard.tsx
@@ -2,22 +2,46 @@ import { useChessContext } from "../contexts/chessContext";
 import "../components/gameboard.css";
 import { Piece, Square } from "../types/gameboard";
 import getActionsForPiece from "../functions/getActionsForPiece";
-import { areSquaresEqual } from "../functions/objectEquality";
+import { arePiecesEqual, areSquaresEqual } from "../functions/objectEquality";
 
 export default function Gameboard() {
   const { gameboard, teamActions, setSelectedAction, pieceActions, setPieceActions } =
     useChessContext();
 
-  function handleSetPieceActions(piece: Piece | null, square: Square) {
-    const actionSquare = pieceActions.find((action) => areSquaresEqual(action.square, square));
-    if (actionSquare) {
-      setSelectedAction(actionSquare);
+  function handleSquarePress(piece: Piece | null, square: Square) {
+    const actionableSquare = pieceActions?.actions.find((action) =>
+      areSquaresEqual(action.square, square)
+    );
+
+    // if clicking on a square thats been highlighted as an actionable square, perform the action
+    if (actionableSquare) {
+      setSelectedAction(actionableSquare);
       return;
     }
+    // if clicking on a square thats not highlighted, highlight the actionable squares for that piece
+    else {
+      //if reclicking the selected square, de-select the piece
+      if (pieceActions && arePiecesEqual(piece, pieceActions.piece)) {
+        setPieceActions(null);
+        return;
+      }
 
-    const actions = getActionsForPiece(piece, teamActions);
-    setPieceActions(actions);
-    console.log(actions);
+      // highlight the actionable squares for the newly clicked piece
+      const actions = getActionsForPiece(piece, teamActions);
+      setPieceActions(actions);
+    }
+  }
+
+  // NEED TO ADD SOME INTERFACE TO ALLOW SELECTION OF PAWN PROMOTION (currently defaults to knight)
+
+  function getSquareOpacity(square: Square): number {
+    if (pieceActions == null) return 1;
+
+    const actionableSquare = pieceActions.actions.some((action) =>
+      areSquaresEqual(action.square, square)
+    );
+
+    return actionableSquare ? 0.3 : 1;
   }
 
   return (
@@ -26,14 +50,8 @@ export default function Gameboard() {
         row.map((piece, colIndex) => (
           <button
             type="button"
-            onClick={() => handleSetPieceActions(piece, { x: rowIndex, y: colIndex })}
-            style={{
-              opacity: pieceActions.some((action) =>
-                areSquaresEqual(action.square, { x: rowIndex, y: colIndex })
-              )
-                ? 0.3
-                : 1,
-            }}
+            onClick={() => handleSquarePress(piece, { x: rowIndex, y: colIndex })}
+            style={{ opacity: getSquareOpacity({ x: rowIndex, y: colIndex }) }}
             key={parseInt(rowIndex.toString() + colIndex.toString())}
             className="square"
           >
