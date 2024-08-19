803366bc936d5237f7c596bace40138a526527fd
Jaari <jaari.rey@gmail.com>
2024-08-19 22:07:20 +1000

    simplified piece/action string

--- src/functions/objectsToString.ts
+++ src/functions/objectsToString.ts
@@ -2,11 +2,9 @@ import { Action, Piece } from "../types/gameboard";
 import squareToAlgebraicNotation from "./squareToAlgebraicNotation";
 
 export function pieceToString(piece: Piece) {
-  return `${piece.teamColour} ${piece.name} at ${squareToAlgebraicNotation(piece.square)}`;
+  return `${piece.name} ${squareToAlgebraicNotation(piece.square)}`;
 }
 
 export function actionToString(action: Action) {
-  return `${pieceToString(action.piece)} - ${action.actionType} ${squareToAlgebraicNotation(
-    action.square
-  )}`;
+  return `${pieceToString(action.piece)} - ${squareToAlgebraicNotation(action.square)}`;
 }
