f83d211f751003450ece0142b57057c5bd119ac9
Jaari <jaari.rey@gmail.com>
2024-09-30 23:28:35 +1000

    removed console.log

--- src/components/Pieces.tsx
+++ src/components/Pieces.tsx
@@ -121,7 +121,6 @@ export default function Pieces() {
         }
       });
 
-      console.log(newStaticPieces);
       setUnmovedPieces(newStaticPieces);
       setMovedPieces([]);
       setPendingFrom([]);
