eee63e9de9289931ec90fab74526cd783613c22b
Jaari <jaari.rey@gmail.com>
2024-09-30 22:15:38 +1000

    reset button hover keyframes

--- src/components/Reset.tsx
+++ src/components/Reset.tsx
@@ -1,15 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useChessContext } from "../contexts/chessContext";
 import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import "./reset.css";
 
 export default function Reset() {
   const { setResetTrigger } = useChessContext();
 
   return (
-    <div style={{ border: "1px solid black" }}>
-      <button title="reset" onClick={() => setResetTrigger((prev) => !prev)}>
-        <FontAwesomeIcon icon={faRotateRight} />
-      </button>
-    </div>
+    <button
+      className="reset-container"
+      title="reset"
+      onClick={() => setResetTrigger((prev) => !prev)}
+    >
+      <FontAwesomeIcon icon={faRotateRight} className="reset-icon" size="3x" />
+    </button>
   );
 }
