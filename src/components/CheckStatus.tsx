866bca76a48f57fb1127602bc0b841c4cc58a951
Jaari <jaari.rey@gmail.com>
2024-11-11 00:51:49 +1000

    added stalemate

--- src/components/CheckStatus.tsx
+++ src/components/CheckStatus.tsx
@@ -3,18 +3,17 @@ import { useChessContext } from "../contexts/chessContext";
 export default function CheckStatus() {
   const { gameboard } = useChessContext();
 
-  const checkedTeam = gameboard.checkedTeamColour;
+  const checkTeam = gameboard.checkTeamColour;
   const checkmateTeam = gameboard.checkmateTeamColour;
 
   function getCheckStatusMessage(): string {
-    let message = "";
     if (checkmateTeam != null) {
-      message = "mate: " + checkmateTeam;
-    } else if (checkedTeam != null) {
-      message = "checked: " + checkedTeam;
-    }
-
-    return message;
+      return "mate: " + checkmateTeam;
+    } else if (gameboard.isStalemate) {
+      return "stalemate";
+    } else if (checkTeam != null) {
+      return "check: " + checkTeam;
+    } else return "";
   }
 
   return <div style={{ border: "1px solid black" }}>{getCheckStatusMessage()}</div>;
