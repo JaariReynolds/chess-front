d0c30c27e84433068a495f83e30b4f42444c1caf
Jaari <jaari.rey@gmail.com>
2024-11-27 01:40:49 +1000

    action history similarly styled to chess.com

--- src/components/ActionHistory.tsx
+++ src/components/ActionHistory.tsx
@@ -1,15 +1,30 @@
 import { useChessContext } from "../contexts/chessContext";
 import "../components/action-history.css";
+import { useEffect, useRef } from "react";
 
 export default function ActionHistory() {
   const { gameboard } = useChessContext();
 
+  const gridContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (gridContainerRef.current)
+      gridContainerRef.current.scrollTop = gridContainerRef.current.scrollHeight;
+  }, [gameboard]);
+
   return (
     <div className="action-history-container">
-      <p>History</p>
-      <div className="grid-container">
+      <div className="grid-container" ref={gridContainerRef}>
         {gameboard.previousActions.map((action, index) => {
-          return <div key={index}>{action}</div>;
+          if (index % 2 == 0) {
+            return (
+              <>
+                <span>{index / 2 + 1}.</span>
+                <span key={index}>{action}</span>
+              </>
+            );
+          }
+          return <span key={index}>{action}</span>;
         })}
       </div>
     </div>
