963d8758a04941d37c524b0b576a4ec538455b49
Jaari <jaari.rey@gmail.com>
2024-12-03 12:54:26 +1000

    console.log removed

--- src/functions/isPawnPromoteAction.ts
+++ src/functions/isPawnPromoteAction.ts
@@ -1,6 +1,5 @@
 import { Action } from "../types/gameboard";
 
 export default function isPawnPromoteAction(action: Action | null): boolean {
-  console.log(action);
   return action != null && action.algebraicNotation.includes("=");
 }
