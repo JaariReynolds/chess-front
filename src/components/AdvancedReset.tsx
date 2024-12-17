ad4cc1a939b1426824a250755ecdd71e2ea1a19f
jaari <jaari.rey@gmail.com>
2024-12-17 19:19:14 +0800

    handleReset now triggers the advancedResetTrigger

--- src/components/AdvancedReset.tsx
+++ src/components/AdvancedReset.tsx
@@ -4,16 +4,18 @@ import "./reset.css";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 export default function AdvancedReset() {
-  const { fenInput, gameboard } = useChessContext();
+  const { fenInput, setFenInput, gameboard, setAdvancedResetTrigger } = useChessContext();
 
   function handleReset() {
-    console.log(fenInput);
+    setFenInput((prev) => prev.trim());
+    setAdvancedResetTrigger((prev) => !prev);
   }
 
   return (
     <button
       className={gameboard.isGameOver ? "reset-container game-over" : "reset-container"}
       title="load from FEN"
+      disabled={fenInput.length == 0}
       onClick={handleReset}
     >
       <FontAwesomeIcon icon={faUpload} size="3x" />
